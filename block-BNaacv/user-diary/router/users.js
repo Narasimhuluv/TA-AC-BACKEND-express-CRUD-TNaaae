bd5685b4d3bad5b18fce6a852a0f7a6f72a39310
Narasimhuluv <vasamnarasimhulu123@gmail.com>
2021-07-15 16:01:26 +0530

    added files

---
+++ block-BNaacv/user-diary/router/users.js
@@ -0,0 +1,22 @@
+let express = require('express')
+let router = express.Router();
+
+router.get('/', (req,res) => {
+    let list = ['Narasimhulu', 'Chinna', 'Sai' , 'Dinesh']
+    res.render('users.ejs',{list : list})
+})
+
+router.get('/singleUser/:id', (req,res) => {
+    res.render('singleUser.ejs', {name : "chinna" , age : 23})
+})
+
+router.get('/users/new', (req,res) => {
+    res.render('userForm.ejs')
+})
+
+router.post('/users/new', (req,res) => {
+    res.send(req.body)
+    console.log(req.body)
+})
+
+module.exports = router
